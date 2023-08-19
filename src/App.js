b9c423984bb155496b85e86391800f8c6b6d8642
MacGregor OBlenes <macgregor@live.ca>
2023-08-18 20:25:49 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/home/Home';
 import Doom from './components/doom/Doom';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className=''>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/doom" element={<Doom />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
